3527ab0b50616ce7078044e2493cd9de5aa84545
Santiago Carrasco <jsanticc@hotmail.com>
2023-08-19 16:29:35 -0700

    chore: add react, vitest and nvm config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
-
 import rome from "astro-rome";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [rome()]
+  integrations: [react(), rome()]
 });
