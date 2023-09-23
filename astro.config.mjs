5e7f02c875e59f1e90820dd72ba98c990c0a27a7
Santiago Carrasco <jsanticc@hotmail.com>
2023-09-23 13:07:14 -0700

    chore: remove rome config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import rome from "astro-rome";
-import { defineConfig } from "astro/config";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [
     react(),
-    rome(),
     tailwind({
       applyBaseStyles: false,
     }),
