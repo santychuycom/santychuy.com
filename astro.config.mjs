3b84b7ccdafc968581f50eedc03743be77b120fe
Santiago Carrasco <jsanticc@hotmail.com>
2024-02-20 18:56:04 -0700

    fix: undo to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://santychuy.com',
 });
