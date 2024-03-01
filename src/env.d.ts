156b8eee6efcece0933748ea8377e29858f1c9cc
Santiago Carrasco <jsanticc@hotmail.com>
2024-03-01 15:32:27 -0700

    chore: add types for env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PLAUSIBLE_TOKEN: string;
+  readonly PLAUSIBLE_SITE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
