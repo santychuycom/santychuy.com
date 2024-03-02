b2d05197ba55ab61879b5ac88b08a796138a93be
Santiago Carrasco <jsanticc@hotmail.com>
2024-03-01 17:56:20 -0700

    build: add env variables on CLI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { loadEnv } from 'vite';
-
-const { PLAUSIBLE_SITE_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
-
-console.log(PLAUSIBLE_SITE_ID);
 
 import cloudflare from '@astrojs/cloudflare';
 
