ca0602c579c7139f07eabafb5d7168fc81e430d0
Santiago Carrasco <jsanticc@hotmail.com>
2024-03-01 17:40:22 -0700

    build: load env variables on config astro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const { PLAUSIBLE_SITE_ID, PLAUSIBLE_TOKEN } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ''
+);
 
 import cloudflare from '@astrojs/cloudflare';
 
@@ -7,4 +14,10 @@ export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://santychuy.com',
+  vite: {
+    define: {
+      PLAUSIBLE_SITE_ID: PLAUSIBLE_SITE_ID,
+      PLAUSIBLE_TOKEN: PLAUSIBLE_TOKEN,
+    },
+  },
 });
