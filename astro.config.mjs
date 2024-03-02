bf1d11a158ce9c435d782c4a90b362b4919e18ce
Santiago Carrasco <jsanticc@hotmail.com>
2024-03-01 17:46:59 -0700

    fix: test load env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 
-const { PLAUSIBLE_SITE_ID, PLAUSIBLE_TOKEN } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  ''
-);
+const { PLAUSIBLE_SITE_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
+console.log(PLAUSIBLE_SITE_ID);
 
 import cloudflare from '@astrojs/cloudflare';
 
@@ -14,10 +12,4 @@ export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://santychuy.com',
-  vite: {
-    define: {
-      PLAUSIBLE_SITE_ID: PLAUSIBLE_SITE_ID,
-      PLAUSIBLE_TOKEN: PLAUSIBLE_TOKEN,
-    },
-  },
 });
