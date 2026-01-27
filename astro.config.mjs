bc74f4d4205289439d50f69c5320ef1a7893eef5
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2026-01-26 22:35:01 -0700

    style: update tailwind and shadcn elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://santychuy.com',
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
