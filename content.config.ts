51dd67208724b74d806c2bcf19d6a80eac8811f2
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2026-02-24 21:01:32 -0700

    feat(content): migrate blog editing config to astro editor

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,1 @@
-import { defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
-import { postSchema } from "./src/schemas/post";
-
-const posts = defineCollection({
-	loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/posts" }),
-	schema: postSchema,
-});
-
-export const collections = { posts };
+export { collections } from "./src/content/config";
